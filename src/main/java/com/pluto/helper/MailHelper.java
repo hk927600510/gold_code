c0d53634721a9c417408967aa6285ee4b81f0928
Kevin.H <Kevin.H@fanruan.com>
2021-12-07 22:49:01 +0800

    修改邮箱密码

--- src/main/java/com/pluto/helper/MailHelper.java
+++ src/main/java/com/pluto/helper/MailHelper.java
@@ -27,12 +27,12 @@ public class MailHelper {
     // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）,
     //     对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
     public static String myEmailAccount = "kevin.h@fanruan.com";
-    public static String myEmailPassword = "h8YnS33h76TLPW2p";
+    public static String myEmailPassword = "QQ3WFFbuSkkrUT6H";
     // 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
     public static String myEmailSMTPHost = "smtp.exmail.qq.com";
 
     // 收件人邮箱（替换为自己知道的有效邮箱）
-    //public static String[] receiveMailAccount = {"927600510@qq.com"};
+    // public static String[] receiveMailAccount = {"927600510@qq.com"};
     public static String[] receiveMailAccount = {"927600510@qq.com", "417983429@qq.com"};
 
     public static void main(String[] args) throws Exception {
